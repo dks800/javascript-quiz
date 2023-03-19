54d099faddb60b319b8a43a76beae208a7bfd4f9
dks800 <deksharma800@gmail.com>
2023-03-19 13:18:51 +0530

    Added Page Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import logo from "./img/js-logo.jpg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./components/User";
+import QuizPage from "./components/QuizPage";
+import Score from "./components/Score";
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        <img src={logo} alt="JS Logo" height={100} width={100} />
-        JavaScript Quiz
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<User />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/score" element={<Score />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
